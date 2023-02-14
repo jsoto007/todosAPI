d30f757354516e88c223464b651421b67d0ef969
Jose Soto <jsoto7087@gmail.com>
2023-02-13 20:42:41 -0500

    added post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get "/categories" do
+    categorization = Categorization.all.limit(10)
+
+    categorization.to_json(
+      include:{tasks: {only: [:description]}}
+    )
   end
+
+  post "/tasks" do 
+    task = Task.create(
+      name: params[:name],
+      description: params[:description],
+      categorization_id: params[:categorization_id]
+    )
+    task.to_json
+  end
+
+
+
+
 
 end
--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -2,5 +2,4 @@
 
 class Categorization < ActiveRecord::Base
     has_many :tasks
-    
 end 
