3c7c52f4e4cb18ccec469c48fbb20e8d0f61b14c
Jose Soto <jsoto7087@gmail.com>
2023-02-17 13:09:22 -0500

    updated get response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     categorization = Categorization.all
 
     categorization.to_json(
-      include:{tasks: {only: [:description, :id]}}
+      include:{tasks: {only: [:description, :id, :created_at, :categorization_id, :updated_at]}}
     )
   end
 
@@ -18,18 +18,28 @@ class ApplicationController < Sinatra::Base
 
   post "/tasks" do 
     #make it using the collection methods
+    # found_categorization = Categorization.find(params[:id])
+    # found_categorization.tasks.create(
+    #   name: params[:name],
+    #   description: params[:description]
+    # )
+
+    # found_categorization.to_json(
+    #   include:{tasks: {only: [:description, :id]}}
+    # )
     task = Task.create(
       name: params[:name],
       description: params[:description],
       categorization_id: params[:categorization_id]
     )
+
     task.to_json
   end
 
 #not using
   delete "/categories/:id" do
     categorization = Categorization.find(params[:id])
-    categorization.Tasks.destroy
+    categorization.tasks.destroy
     categorization.destroy
     #also destroy all dependencies
     # categorization.to_json
