3e652fdf47bf51ab690ba61f8780e7a1a3dc21fd
Jose Soto <jsoto7087@gmail.com>
2023-02-20 18:36:34 -0500

    updated delete categorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,30 +10,34 @@ class ApplicationController < Sinatra::Base
     )
   end
 
-  post "/tasks" do 
-    #make it using the collection methods
-    # found_categorization = Categorization.find(params[:id])
-    # found_categorization.tasks.create(
-    #   name: params[:name],
-    #   description: params[:description]
-    # )
+  get "/categories/:id" do
+    categorization = Categorization.find(params[:id])
+    categorization.to_json(
+      include: :tasks
+    )
+  end
 
-    # found_categorization.to_json(
-    #   include:{tasks: {only: [:description, :id]}}
-    # )
+  post "/categories" do 
+    categorisation = Categorization.create(
+      name: params[:name]
+    )
+    categorisation.to_json(
+      include: :tasks
+    )
+  end 
+
+  post "/tasks" do 
     task = Task.create(
       name: params[:name],
       description: params[:description],
       categorization_id: params[:categorization_id]
     )
-
     task.to_json
   end
 
-#not using
   delete "/categories/:id" do
     categorization = Categorization.find(params[:id])
-    categorization.tasks.destroy
+    categorization.tasks.destroy_all
     categorization.destroy
   end 
 
